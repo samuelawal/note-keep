c6a41a2d3651034824fd2f1680c7d111e641c1f2
Samuel Lawal <samuellawal60@gmail.com>
2022-03-17 20:38:48 +0100

    Added the form layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,18 @@
 <template>
-  <main>Blank page</main>
+  <main>
+    <div class="container p-5">
+      <form>
+        <h1 class="text-center fw-bold">Note~Keep <i class="fa-solid fa-book ps-3"/></h1>
+        <label for="addNote" class="form-label">Add Note..</label>
+      <textarea type="password" id="addNote" class="form-control"/> 
+      <button class="btn btn-primary my-3 form-control border-0"><i class="fa-solid fa-plus pe-3"></i> Add Note</button>
+      </form>
+    </div>
+  </main>
 </template>
+<style scoped>
+.btn {
+  background-color: rgb(5, 170, 5);
+}
+</style>
+
